9a6fa556c6aae78f28b7861befca36d2e886bc29
NakajimaTakahiko <t_nakajima@lermo.co.jp>
2020-02-29 18:39:40 +0900

    firebaseプロジェクトを作成した

--- functions/index.js
+++ functions/index.js
@@ -1,43 +1,44 @@
 const functions = require('firebase-functions');
-const fetch = require('node-fetch')
-const request = require('request');
-const nodemailer = require('nodemailer');
-const gmailEmail = functions.config().gmail.email
-const gmailPassword = functions.config().gmail.password
-const mailTransport = nodemailer.createTransport({
-  service: 'gmail',
-  port: 46,
-  secure: true,
-  auth: {
-    user: gmailEmail,
-    pass: gmailPassword
-  }
-});
+const fetch = require('node-fetch');
 
-// メール送信関数
-exports.get = functions.pubsub.schedule('every 1 minutes').timeZone('Asia/Tokyo').onRun((context) => {
-  console.log('ビットコイン価格取得開始')
-  const url = 'https://bitflyer.com/api/echo/price';
-  request(url, (err, response, payload) => {
-    console.log(payload);
+// const request = require('request');
+// const nodemailer = require('nodemailer');
+// const gmailEmail = functions.config().gmail.email
+// const gmailPassword = functions.config().gmail.password
+// const mailTransport = nodemailer.createTransport({
+//   service: 'gmail',
+//   port: 46,
+//   secure: true,
+//   auth: {
+//     user: gmailEmail,
+//     pass: gmailPassword
+//   }
+// });
 
-    console.log('メール送信開始')
-    let email = {
-      from: gmailEmail,
-      to: gmailEmail,
-      subject: 'メール送信テスト',
-      text: payload.mid
-    }
-    mailTransport.sendMail(email, (err, info) => {
-      if (err) {
-        return console.log(err)
-      }
-      return console.log('メール送信終了');
-    })
-  })
-  console.log('ビットコイン価格取得終了')
-  return null;
-});
+// // メール送信関数
+// exports.get = functions.pubsub.schedule('every 1 minutes').timeZone('Asia/Tokyo').onRun((context) => {
+//   console.log('ビットコイン価格取得開始')
+//   const url = 'https://bitflyer.com/api/echo/price';
+//   request(url, (err, response, payload) => {
+//     console.log(payload);
+
+//     console.log('メール送信開始')
+//     let email = {
+//       from: gmailEmail,
+//       to: gmailEmail,
+//       subject: 'メール送信テスト',
+//       text: payload.mid
+//     }
+//     mailTransport.sendMail(email, (err, info) => {
+//       if (err) {
+//         return console.log(err)
+//       }
+//       return console.log('メール送信終了');
+//     })
+//   })
+//   console.log('ビットコイン価格取得終了')
+//   return null;
+// });
 
 // ビットコインの価格を取得する関数
 // 本当は毎分実行するやつにするけど、とりあえず任意のタイミングでやるやつで作成
